0ce50b63e21c2346a63a1e2bd3e7f71a8cd0800a
Nishanth Mane <nishanth.mane@rapyuta-robotics.com>
2019-11-27 11:03:18 +0530

    Add resize observer polyfill version and name

--- hugo/static/js/ResizeObserver.js
+++ hugo/static/js/ResizeObserver.js
@@ -1,3 +1,4 @@
+/* resize-observer-polyfill@1.5.1*/
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
